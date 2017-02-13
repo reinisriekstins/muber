d42e28076aa14985e002b86fc94e840552c8038d
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-02-13 15:55:25 +0200

    added error response middleware

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   routes = require('./routes/routes'),
+  mongoose = require('mongoose'),
   app = express()
+
+mongoose.Promise = global.Promise
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber')
+}
 
 app.use(bodyParser.json())
 
 routes(app)
+
+app.use((err, req, res, next) => {
+  if (err)
+    res.status(422).send({ error: err.message })
+
+  next()
+})
 
 module.exports = app
